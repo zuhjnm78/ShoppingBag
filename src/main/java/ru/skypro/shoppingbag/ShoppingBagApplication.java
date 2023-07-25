4ab38075311ccd4fb6739d1b8b3689231206099a
oleghenkel <oleg.henkel75@gmail.com>
2023-07-25 19:04:41 +0200

    HW Shopping Bag

---
+++ src/main/java/ru/skypro/shoppingbag/ShoppingBagApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.shoppingbag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingBagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/shoppingbag/ShoppingBagApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.shoppingbag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
